14dc97c954f8699d333176de12b8d57446adb451
AnqiAngelineWang <anqi.angeline.wang@gmail.com>
2019-09-27 19:41:41 -0400

    Implementation of HTTPPost, HTTPRequestModule and ResponseReader

--- src/main/java/httpclientlibrary/HTTPRequestModule.java
+++ src/main/java/httpclientlibrary/HTTPRequestModule.java
@@ -1,18 +1,112 @@
 package httpclientlibrary;
 
+import java.util.HashMap;
+
+/**
+ *
+ */
 public class HTTPRequestModule {
+
     private
         String HTTPMethods;
         String HTTPVersion = "HTTP/1.0\r\n";
+        String Host;
         String URL;
         String ConnectionStatus = "Connection: Keep-Alive\r\n";
+        String ContentLength;
+        HashMap<String, String> HeaderMap = new HashMap<>();
+        String Body;
 
-    public void setMethodAndURL(String method, String url){
+    /**
+     *
+     * @param method
+     */
+    public void setMethod(String method){
         this.HTTPMethods = method;
-        this.URL = url;
     }
 
+    /**
+     *
+     * @param key
+     * @param value
+     */
+    public void addHeader(String key, String value){
+        HeaderMap.put(key, value);
+    }
+
+    /**
+     *
+     * @param content
+     */
+    public void setBody(String content){
+        this.ContentLength = String.valueOf(content.length());
+        this.Body = content;
+    }
+
+    /**
+     *
+     * @return
+     */
     public String printRequest(){
         return HTTPMethods + " " + URL + " " + HTTPVersion + ConnectionStatus +"\r\n";
+    }
+
+    /**
+     *
+     * @param method
+     * @param args
+     * @return
+     */
+    public static HTTPRequestModule requestBuilder(String method, String[] args){
+
+        HTTPRequestModule request = new HTTPRequestModule();
+
+        // Set request method
+        request.setMethod(method);
+
+        for(int i=2;i<args.length;i++){
+            if(args[i].contains("http://")){
+                request.URL = args[i].replaceAll("'","");
+            }else{
+                System.out.println("URL not found");
+                return null;
+            }
+        }
+
+        for(int i=2;i<args.length;i++){
+            // check "-h" parameter
+            if(args[i].equals("-h")){
+                String pair = args[i+1];
+                if(!pair.contains(":")){
+                    System.out.println("invalid parameter format");
+                    return null;
+                }
+                String[] param = pair.split(":", 2);
+                request.addHeader(param[0], param[1]);
+            }
+
+            if(request.HTTPMethods.equals("POST")){
+                // check "-d" parameter
+                if(args[i].equals("-d")){
+                    String data = args[i+1];
+                    if(!data.contains("'")){
+                        System.out.println("invalid inline data format");
+                        return null;
+                    }
+                    request.setBody(data);
+
+                }else if(args[i].equals("-f")){
+                    // Either "-d" or "-f" can be used but not both.
+                    String filePath = args[i+1];
+                    if(!filePath.contains("'")){
+                        System.out.println("invalid inline data format");
+                        return null;
+                    }
+                    request.setBody(filePath);
+                }
+            }
+
+        }
+        return request;
     }
 }
--- src/main/java/httpclientlibrary/ResponseReader.java
+++ src/main/java/httpclientlibrary/ResponseReader.java
@@ -2,50 +2,39 @@ package httpclientlibrary;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
 
 public class ResponseReader {
 
-    // readFully reads until the request is fulfilled or the socket is closed
-    public static void readResponse(SocketChannel channel, String args[]) throws IOException {
+    // readResponse reads the response from the server and prepares output format
+    public static void readResponse(String content, String args[]){
 
-        // Need to be improved
-        ByteBuffer buf = ByteBuffer.allocate(2048);
-        StringBuffer stringBuf =new StringBuffer();
-
-        channel.read(buf);
-        buf.flip();
-
-        while(buf.hasRemaining()){
-            stringBuf.append((char)buf.get());
-        }
-
-        String completeResponse = stringBuf.toString().trim();
-        String[] split = completeResponse.split("\r\n\r\n");
+        String HTTPResponse = content.trim();
+        String[] split = HTTPResponse.split("\r\n\r\n");
         String print = split[1];
 
+        // For loop the arguments and do corresponding function.
         for(int i=0;i<args.length;i++){
 
             if(args[i].equals("-v")){
-                print = completeResponse;
+                print = HTTPResponse;
             }if(args[i].equals("-o")){
                 byte[] sourceByte = split[1].getBytes();
-                if(null != sourceByte) {
-                    try {
-                        File file = new File("./"+args[i+1]);        //文件路径（路径+文件名）
-                        if (!file.exists()) {    //文件不存在则创建文件，先创建目录
-                            File dir = new File(file.getParent());
-                            dir.mkdirs();
-                            file.createNewFile();
-                        }
-                        FileOutputStream outStream = new FileOutputStream(file);    //文件输出流用于将数据写入文件
-                        outStream.write(sourceByte);
-                        outStream.close();    //关闭文件输出流
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                try {
+                    // If the file not exist, create it at the root
+                    File file = new File("./"+args[i+1]);
+                    /*
+                    if (!file.exists()) {
+                        File dir = new File(file.getParent());
+                        dir.mkdirs();
+                        file.createNewFile();
                     }
+                    */
+                    // FileOutputStream to write in file
+                    FileOutputStream outStream = new FileOutputStream(file);
+                    outStream.write(sourceByte);
+                    outStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
                 return;
             }
